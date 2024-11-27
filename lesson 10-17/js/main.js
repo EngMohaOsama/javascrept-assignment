2ff821685f5a338bf4fd0491a50f48faa4e175d4
Osama <eng13mohamedosama@gmail.com>
2024-11-27 10:07:10 +0200

    finishing lesson10-17 tast 1 & 3 & 4

---
+++ lesson 10-17/js/main.js
@@ -0,0 +1,24 @@
+let a = "10";
+let b = "20";
+console.log(a + b); // Normal Concatenate => 1020
+console.log(typeof (a + b)); // Normal Concatenate => String
+console.log(`${a}${b}`); // Template Literals Way => 1020
+console.log(typeof`${a}${b}`); // Template Literals Way => String
+
+console.log("Normal Concatenate\n" + b + "\n" + a);
+/*
+  Normal Concatenate
+  20
+  10
+*/
+
+console.log(`
+"Normal Concatenate"
+${b}
+${a}
+`);
+/*
+  Template Literals Way
+  20
+  10
+*/
