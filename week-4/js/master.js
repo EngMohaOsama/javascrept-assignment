22f15ccf64b17bd32ca03a93abf4a9abe1f6633a
Osama <eng13mohamedosama@gmail.com>
2024-12-01 10:42:59 +0200

    finishing week-4 GLI-102

---
+++ week-4/js/master.js
@@ -0,0 +1,16 @@
+// Examples
+console.log(100_000); // 100000
+console.log(100000); // 100000
+console.log(5e4 + 5e4); // 100000
+
+// Your Solutions
+console.log(1000 * 100); // 100000
+console.log(10_000 * 10); // 100000
+console.log(50_000 + 50_000); // 100000
+console.log(25_000 + 25_000 + 25_000 + 25_000); // 100000
+console.log(1_000_000 / 10); // 100000
+console.log(Math.pow(10, 5)); // 100000
+console.log(5000 * 20); // 100000
+console.log(+"100000"); // 100000
+console.log(200000 - 100000); // 100000
+console.log(-100000 * -1); // 100000
---
+++ week-4/js/master3.js
@@ -0,0 +1,1 @@
+console.log(Number.MAX_SAFE_INTEGER - (Number.MAX_SAFE_INTEGER - 16)); // 16
