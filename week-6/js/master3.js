d2afdf44ce9c38c9d0f000b266c5a949a219bb22
Osama <eng13mohamedosama@gmail.com>
2024-12-03 13:06:29 +0200

    changing week-6 master3.js GLI-104

--- week-6/js/master3.js
+++ week-6/js/master3.js
@@ -2,6 +2,6 @@ let arrOne = ["C", "D", "X"];
 let arrTwo = ["A", "B", "Z"];
 let finalArr = [];
 
-finalArr = [...arrTwo, ...arrOne].sort().reverse();
+finalArr = arrOne.concat(arrTwo).sort().reverse();
 
 console.log(finalArr); // ["Z", "X", "D", "C", "B", "A"]
