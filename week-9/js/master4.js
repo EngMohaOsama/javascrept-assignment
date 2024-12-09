c9a5bab5f9f642ecb44217049259737b5c46e0fa
Osama <eng13mohamedosama@gmail.com>
2024-12-09 13:01:44 +0200

    finishing week-9 GLI-121

--- week-9/js/master4.js
+++ week-9/js/master4.js
@@ -1,33 +1,17 @@
 function specialMix(...data) {
   let sum = 0;
-  let allStrings = true;
 
   for (let item of data) {
-    if (typeof item === "number") {
-      sum += item;
-      allStrings = false;
-    } else if (typeof item === "string") {
-      parseInt(item);
-      sum += item;
-      //   let numbersInString = item.match(/\d+/g);
-      //   console.log(numbersInString);
-      //   if (numbersInString) {
-      //     sum += numbersInString.reduce((acc, num) => acc + parseInt(num, 10), 0);
-      allStrings = false;
-      //   }
-    }
+    let num = parseInt(item);
+    if (!isNaN(num)) sum += num;
   }
 
-  if (allStrings) {
-    return "All Is Strings";
-  }
+  if (sum == 0) return "All Is Strings";
 
   return sum;
 }
 
 console.log(specialMix(10, 20, 30)); // 60
-console.log(specialMix("10Test20", "Testing", "20Cool")); // 30
+console.log(specialMix("10Test", "Testing", "20Cool")); // 30
 console.log(specialMix("Testing", "10Testing", "40Cool")); // 50
 console.log(specialMix("Test", "Cool", "Test")); // All Is Strings
-
-console.log(parseInt("dsadas20"));
