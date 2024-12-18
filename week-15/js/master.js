27d117a9d17628c5af0dfb4b448c18de0f9d1b3f
Osama <eng13mohamedosama@gmail.com>
2024-12-18 11:52:23 +0200

    adjustment week-15 GLI-142

--- week-15/js/master.js
+++ week-15/js/master.js
@@ -3,6 +3,16 @@ let font = document.querySelector("#font");
 let color = document.querySelector("#color");
 let size = document.querySelector("#size");
 
+// for saving change on (body) in case the web get reload or closed Buy getting value form local storage. 
+document.body.style.fontFamily = window.localStorage.getItem("font");
+document.body.style.color = window.localStorage.getItem("color");
+document.body.style.fontSize = window.localStorage.getItem("size");
+
+// for saving change of (input fled) in case the web get reload or closed Buy getting value form local storage. 
+font.value = window.localStorage.getItem("font") || font.options[0].text;
+color.value = window.localStorage.getItem("color") || color.options[0].text;
+size.value = window.localStorage.getItem("size") || size.options[0].text;
+
 font.onchange = function () {
     window.localStorage.setItem("font", this.value);
     document.body.style.fontFamily = this.value;
@@ -15,13 +25,3 @@ size.onchange = function () {
     window.localStorage.setItem("size", this.value);
     document.body.style.fontSize = this.value;
 }
-
-// for saving change on (body) in case the web get reload or closed Buy getting value form local storage. 
-document.body.style.fontFamily = window.localStorage.getItem("font");
-document.body.style.color = window.localStorage.getItem("color");
-document.body.style.fontSize = window.localStorage.getItem("size");
-
-// for saving change of (input fled) in case the web get reload or closed Buy getting value form local storage. 
-font.value = window.localStorage.getItem("font");
-color.value = window.localStorage.getItem("color");
-size.value = window.localStorage.getItem("size");
