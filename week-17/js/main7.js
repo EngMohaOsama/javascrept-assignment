db96ed6cc29f0d8132f7e92df22093445d2a048c
Osama <eng13mohamedosama@gmail.com>
2024-12-23 15:09:55 +0200

    update main7.js file

--- week-17/js/main7.js
+++ week-17/js/main7.js
@@ -7,10 +7,13 @@ console.log(numArray);
 let numArray2 = [...numOne, ...numTwo];
 console.log(numArray2);
 
-let numArray3 = numOne
-for (i = 0; i < numTwo.length; i++) {
-    numArray3.push(numTwo[i]);
-}
+let numArray3 = [...numOne]
+// for (i = 0; i < numTwo.length; i++) {
+//     numArray3.push(numTwo[i]);
+// }
+numTwo.forEach(num => {
+    numArray3.push(num)
+});
 console.log(numArray3);
 // Needed Output
 // [1, 2, 3, 4, 5, 6]
